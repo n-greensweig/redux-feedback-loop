9f39e0fdcf983d4517929d2e258690fea9697199
Noah Greensweig <ngreensweig@gmail.com>
2023-11-10 16:35:53 -0600

    added feeling and understanding components and routes to the app

---
+++ src/components/Feeling/Feeling.jsx
@@ -0,0 +1,18 @@
+import { TextField } from "@mui/material";
+import NextButton from "../NextButton/NextButton";
+
+function Feeling() {
+
+    return (
+        <>
+            <h1>How are you feeling today?</h1>
+            <TextField type="number"
+                label='Feeling?'
+            />
+            <NextButton />
+        </>
+    )
+
+}
+
+export default Feeling;
