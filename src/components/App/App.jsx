4cbbe53d726617f6bbe23e3844383008b58c5957
Noah Greensweig <ngreensweig@gmail.com>
2023-11-10 14:44:12 -0600

    added redux store

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+
+  const feeling = useSelector(store => store.feeling);
+  console.log(feeling);
+
+  const dispatch = useDispatch();
 
   return (
     <div className='App'>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+import { Provider } from 'react-redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger';
+
+const feeling = (state = null, action) => {
+    return state;
+};
+
+const reduxStore = createStore(
+    combineReducers({
+        feeling,
+    }),
+    applyMiddleware(logger)
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={reduxStore}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
