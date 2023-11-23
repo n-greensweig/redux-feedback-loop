d860ca16281fd326cd85d2d5c93850f67e834f5d
Noah Greensweig <ngreensweig@gmail.com>
2023-11-23 08:23:59 -0600

    added sortability to MUI table

--- src/components/FeedbackTable/FeedbackTable.jsx
+++ src/components/FeedbackTable/FeedbackTable.jsx
@@ -1,14 +1,22 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TableSortLabel } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 function FeedbackTable() {
 
     const [responses, setResponses] = useState([]);
+    const [sortOrder, setSortOrder] = useState('asc');
+    const [sortedColumn, setSortedColumn] = useState('id');
 
-    // GET request displaying the existing feedback in an MUI table,
-    // with the most recent feedback appearing at the top of the list
-    // (sorted by id DESC)
+    const columns = [
+        { id: 'name', label: 'Respondent' },
+        { id: 'feeling', label: 'Feeling' },
+        { id: 'understanding', label: 'Understanding' },
+        { id: 'support', label: 'Support' },
+        { id: 'comments', label: 'Comments' },
+    ];
+
+    // GET request to display feedback sorted by id DESC
     const getFeedback = () => {
         axios.get('/feedback')
             .then(response => {
@@ -24,23 +32,67 @@ function FeedbackTable() {
         getFeedback();
     }, []);
 
+    const handleSort = columnId => {
+        const isAsc = sortedColumn === columnId && sortOrder === 'asc';
+        setSortOrder(isAsc ? 'desc' : 'asc');
+        setSortedColumn(columnId);
+    };
+
+    const sortedResponses = [...responses].sort((a, b) => {
+
+        const columnA = a[sortedColumn];
+        const columnB = b[sortedColumn];
+
+        const isNumeric = value => !isNaN(parseFloat(value)) && isFinite(value);
+
+        const isNumericA = isNumeric(columnA);
+        const isNumericB = isNumeric(columnB);
+
+        if (isNumericA && isNumericB) {
+
+            if (sortOrder === 'asc') {
+                return parseFloat(columnA) - parseFloat(columnB);
+            } else {
+                return parseFloat(columnB) - parseFloat(columnA);
+            }
+
+        } else {
+            if (typeof columnA === 'string' && typeof columnB === 'string') {
+
+                if (sortOrder === 'asc') {
+                    return columnA.localeCompare(columnB);
+                } else {
+                    return columnB.localeCompare(columnA);
+                }
+            } else {
+                return 0;
+            }
+        }
+    });
+
     return (
         <>
             <h1>Admin</h1>
-            <TableContainer component={Paper} style={{margin: '20px', maxHeight: '400px', overflowY: 'auto'}}>
-                <Table aria-label="simple table">
+            <TableContainer component={Paper} style={{ margin: '20px', maxHeight: '400px', overflowY: 'auto' }}>
+                <Table stickyHeader aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Respondent</TableCell>
-                            <TableCell>Feeling</TableCell>
-                            <TableCell>Understanding</TableCell>
-                            <TableCell>Support</TableCell>
-                            <TableCell>Comments</TableCell>
+                            {columns.map((column) => (
+                                <TableCell key={column.id}>
+                                    <TableSortLabel
+                                        active={sortedColumn === column.id}
+                                        direction={sortOrder}
+                                        onClick={() => handleSort(column.id)}
+                                    >
+                                        {column.label}
+                                    </TableSortLabel>
+                                </TableCell>
+                            ))}
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {responses.map(response => (
-                            <TableRow>
+                        {sortedResponses.map(response => (
+                            <TableRow key={response.id}>
                                 <TableCell>{response.name}</TableCell>
                                 <TableCell>{response.feeling}</TableCell>
                                 <TableCell>{response.understanding}</TableCell>
