cad71c5e96a3b4558e0c8fc7ba20ab8857862fa6
Noah Greensweig <ngreensweig@gmail.com>
2023-11-24 08:27:39 -0600

    added Roboto font and CssBaseline to app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+import { CssBaseline } from '@mui/material';
+import '@fontsource/roboto';
+
 import { Provider } from 'react-redux';
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
@@ -111,6 +114,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={reduxStore}>
+            <CssBaseline />
             <App />
         </Provider>
     </React.StrictMode>
